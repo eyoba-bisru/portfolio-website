7c79d9860cb25a3c9e9f97f472a4973306b3e466
Eyob <60362517+eyoba-bisru@users.noreply.github.com>
2025-10-30 14:36:19 +0300

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -57,7 +57,7 @@ export default function Intro() {
         <span className="font-bold">3 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span> &{" "}
-        <span className="underline">Django (DRF)</span>.
+        <span className="underline">Golang</span>.
       </motion.h1>
 
       <motion.div
